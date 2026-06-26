c475e0fef9b2072c50209a2c747a54cd55cb0eb8
Diego <88157099+Astelee@users.noreply.github.com>
2026-06-26 09:06:02 -0300

    Create LuzPlugin.java

---
+++ src/main/java/com/newscraft/luzplugin/LuzPlugin.java
@@ -0,0 +1,19 @@
+package com.newscraft.luzplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LuzPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("LuzPlugin | NewsCraft ativado com sucesso!");
+        LuzCommand luzCommand = new LuzCommand(this);
+        getCommand("luz").setExecutor(luzCommand);
+        getCommand("luz").setTabCompleter(luzCommand);
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("LuzPlugin | NewsCraft desativado.");
+    }
+}
